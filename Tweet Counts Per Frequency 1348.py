020db974223b0acc787c2c448ef356c4d05cb48c
Tung Le <ttungl@gmail.com>
2020-02-26 09:34:19 -0800

    comment#2

--- Tweet Counts Per Frequency 1348.py
+++ Tweet Counts Per Frequency 1348.py
@@ -7,11 +7,11 @@
 
 # Stores the tweetName at the recorded time (in seconds).
 
-2. getTweetCountsPerFrequency(string freq, string tweetName, int startTime, int endTime)
+# 2. getTweetCountsPerFrequency(string freq, string tweetName, int startTime, int endTime)
 
-Returns the total number of occurrences for the given tweetName per minute, hour, or day (depending on freq) starting from the startTime (in seconds) and ending at the endTime (in seconds).
-freq is always minute, hour or day, representing the time interval to get the total number of occurrences for the given tweetName.
-The first time interval always starts from the startTime, so the time intervals are [startTime, startTime + delta*1>,  [startTime + delta*1, startTime + delta*2>, [startTime + delta*2, startTime + delta*3>, ... , [startTime + delta*i, min(startTime + delta*(i+1), endTime + 1)> for some non-negative number i and delta (which depends on freq).  
+# Returns the total number of occurrences for the given tweetName per minute, hour, or day (depending on freq) starting from the startTime (in seconds) and ending at the endTime (in seconds).
+# freq is always minute, hour or day, representing the time interval to get the total number of occurrences for the given tweetName.
+# The first time interval always starts from the startTime, so the time intervals are [startTime, startTime + delta*1>,  [startTime + delta*1, startTime + delta*2>, [startTime + delta*2, startTime + delta*3>, ... , [startTime + delta*i, min(startTime + delta*(i+1), endTime + 1)> for some non-negative number i and delta (which depends on freq).  
  
 
 Example:
